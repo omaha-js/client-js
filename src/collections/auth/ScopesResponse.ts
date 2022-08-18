702d459a19d169c26fb4806087d578a046ea59aa
Bailey Herbert <hello@bailey.sh>
2022-08-17 20:46:04 -0700

    Add groups to scopes list response

--- src/collections/auth/ScopesResponse.ts
+++ src/collections/auth/ScopesResponse.ts
@@ -1,10 +1,13 @@
+import { CollaborationRole } from '../../entities/enum/CollaborationRole';
 import { Scope } from '../../types/scopes';
 
 export interface ScopesResponse {
 	scopes: ScopeDescriptor[];
 }
 
-export interface ScopeDescriptor {
+export type ScopeDescriptor = BaseScopeDescriptor & (RepoScopeDescriptor | AccountScopeDescriptor);
+
+interface BaseScopeDescriptor {
 
 	/**
 	 * The ID of the scope.
@@ -22,13 +25,31 @@ export interface ScopeDescriptor {
 	description: string;
 
 	/**
-	 * Whether the scope applies to accounts or repositories.
-	 */
-	type: 'account' | 'repo';
-
-	/**
 	 * A boolean indicating whether the current user has access to this scope.
 	 */
 	active: boolean;
+
+}
+
+interface RepoScopeDescriptor {
+
+	/**
+	 * Whether the scope applies to accounts or repositories.
+	 */
+	type: 'repo';
+
+	/**
+	 * For repository scopes, this will be an array of collaboration roles which automatically inherit this scope.
+	 */
+	groups: CollaborationRole[];
+
+}
+
+interface AccountScopeDescriptor {
+
+	/**
+	 * Whether the scope applies to accounts or repositories.
+	 */
+	type: 'account';
 
 }
